29cad1a161bc3eeee4a28becf5018330f8e007e4
Stefano Grasselli <sg@zextras.com>
2017-07-13 12:48:06 +0200

    Fixed rename file input on show and change tab

--- zimlet/src/ZimbraDriveController.ts
+++ zimlet/src/ZimbraDriveController.ts
@@ -241,6 +241,7 @@ export class ZimbraDriveController extends ZmListController {
 
     let elements = this.getViewElements(this._currentViewId, this._parentView[this._currentViewId]);
 
+    this.resetRenameFile();
     this._setView({
       view: this._currentViewId,
       viewType: this._currentViewType,
@@ -692,7 +693,7 @@ export class ZimbraDriveController extends ZmListController {
         this._sendRenameRequest(fileName, item);
       }
     } else {
-      this._listView[this._currentViewId].redrawItem(item);
+      this._redrawItem(item);
     }
   }
 
@@ -747,6 +748,11 @@ export class ZimbraDriveController extends ZmListController {
     this.resetRenameFile();
     this._listView[this._currentViewId].redrawItem(item);
   }
+
+  public _preHideCallback(): void {
+    this.resetRenameFile();
+    return ZmController.prototype._preHideCallback.call(this);
+  };
 
   public _checkDuplicate(name: string): boolean {
     name = name.toLowerCase();
